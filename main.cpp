8e252a9d09f159c0c2b64fb999cbd26c2fc3c67f
Egor200313 <burdin.es@phystech.edu>
2022-03-31 20:09:06 +0300

    add interface

--- main.cpp
+++ main.cpp
@@ -3,10 +3,29 @@
 #include <filesystem>
 
 int main() {
-    std::string filename = std::string(std::filesystem::current_path()) + "/../testfiles/simple.txt";
-    std::cout << filename;
+    std::string filename;
+    int queries = 0;
+    std::cout << "Enter file full path: ";
+    std::cin >> filename;
+    if (!std::filesystem::exists(filename)) {
+        std::cout << "File doesn't exist!\n";
+        return 0;
+    }
+    std::cout << "Enter number of queries: ";
+    std::cin >> queries;
     solution sol(filename);
-    std::cout << sol.containsStr("hello") << std::endl;
+    std::string query;
+    for (int i = 0; i < queries; ++i) {
+        std::cout << "Enter string to search: ";
+        std::cin >> query;
+
+        auto position = sol.containsStr(query);
+        if (position != -1) {
+            std::cout << "String position: " << position << std::endl;
+        } else {
+            std::cout << "String is not found\n";
+        }
+    }
     return 0;
 }
 
